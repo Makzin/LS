f29c43e751dc56b9418206958075bebd8e4a8c52
Max Van Lyl <maximvanlyl@gmail.com>
2017-05-10 21:57:54 -0400

    refactored calculator app to use yaml config file for messaging

--- 101_programming_foundations/Lesson 2/calculator.rb
+++ 101_programming_foundations/Lesson 2/calculator.rb
@@ -3,16 +3,27 @@
 # perform the operation on the 2 numbers
 # output the result
 
+require 'pry'
+require 'yaml'
+
+MESSAGES = YAML.load_file('calculator_messages.yml')
+puts MESSAGES.inspect
+
+
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
 def valid_number?(num)
-  num.to_i() != 0
+  /^\d+$/.match(num) #regex solution to check if param passed is valid integer
 end
 
+def number?(num)
+  num.to_i.integer? || num.to_f.float?
+end 
+
 def operation_to_message(op)
-  case op
+  word = case op
   when '1'
     'Adding'
   when '2'
@@ -22,57 +33,53 @@ def operation_to_message(op)
   when '4'
     'Dividing'
   end
+
+  'random line of code'
+  word
 end
 
-prompt('Welcome to calculator! Please enter your name:')
+prompt(MESSAGES['welcome'])
 
 name = ''
 loop do
   name = Kernel.gets.chomp
 
   if name.empty?
-    prompt('Make sure to use a valid name.')
+    prompt(MESSAGES['valid_name'])
   else
     break
   end
 end
 
-prompt("Hi #{name}!")
+prompt(MESSAGES['greeting'])
 
 loop do # main loop
   number1 = ''
   number2 = ''
 
   loop do
-    prompt("What's your first number?")
+    prompt(MESSAGES['first_number'])
     number1 = Kernel.gets().chomp()
 
-    if valid_number?(number1)
+    if number?(number1)
       break
     else
-      prompt("Invalid number! Please try again")
+      prompt(MESSAGES['invalid_number'])
     end
   end
 
   loop do
-    prompt("What's your second number?")
+    prompt(MESSAGES['second_number'])
     number2 = Kernel.gets().chomp()
 
-    if valid_number?(number2)
+    if number?(number2)
       break
     else
-      prompt("Invalid number! Please try again")
+      prompt(MESSAGES['invalid_number'])
     end
   end
 
-  operator_prompt = <<-MSG
-    What operation would you like to perform?
-    1) Add
-    2) Subtract
-    3) Multiply
-    4) Divide
-  MSG
-  prompt(operator_prompt)
+  prompt(MESSAGES['operator_prompt'])
 
   operator = ''
   loop do
@@ -81,7 +88,7 @@ loop do # main loop
     if %w(1 2 3 4).include?(operator)
       break
     else
-      prompt('Must choose 1, 2, 3 or 4')
+      prompt(MESSAGES['invalid_prompt'])
     end
   end
 
@@ -99,11 +106,11 @@ loop do # main loop
       number1.to_f / number2.to_f
     end
 
-  prompt("The result is #{result}")
+  prompt(MESSAGES['result'])
+  prompt(MESSAGES['another_calculation?'])
 
-  prompt('Do you want to perform another calculation? (Y to confirm)')
   answer = Kernel.gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt('Thank you for using calculator. Good bye!')
+prompt(MESSAGES['goodbye'])
