fa822676e005645117c023314aca8ac036c5bed6
Max Van Lyl <maximvanlyl@gmail.com>
2017-06-27 12:35:54 -0400

    more easy 3 ruby problems updated email for commits

--- 101_109_small_problems/easy3/easy3_10.rb
+++ 101_109_small_problems/easy3/easy3_10.rb
@@ -2,9 +2,31 @@
 
 # Examples:
 
-real_palindrome?('madam') == true
-real_palindrome?('Madam') == true           # (case does not matter)
-real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
-real_palindrome?('356653') == true
-real_palindrome?('356a653') == true
-real_palindrome?('123ab321') == false
+def real_palindrome?(input)
+  alphabet_array = %w(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z)
+  numbers_array =  %w(1 2 3 4 5 6 7 8 9 0)
+
+  cleaned_input_array = input.split('').select do |element|
+    alphabet_array.include?(element) || numbers_array.include?(element)
+  end 
+
+  filtered_string = cleaned_input_array.join
+
+  if input == '123ab321'
+    p filtered_string.downcase 
+    p filtered_string.downcase.reverse
+  end 
+
+  filtered_string.downcase.reverse == filtered_string.downcase
+end 
+
+
+p real_palindrome?('madam') == true
+p real_palindrome?('Madam') == true           # (case does not matter)
+p real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
+p real_palindrome?('356653') == true
+p real_palindrome?('356a653') == true
+p real_palindrome?('123ab321') == false
+
+#output from this code is 'true' on all cases, even the last one. When debugging the '123ab321' it's clear that this produces 2 different strings, but somehow the code returns 'true' on it. 
+
