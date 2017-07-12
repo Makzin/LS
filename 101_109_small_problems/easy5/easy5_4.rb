d19d99cbdce05caec7bec7829a4243960ef052a6
Max Van Lyl <maximvanlyl@gmail.com>
2017-07-12 07:33:13 -0400

    more ex 5 updated email for commits

--- 101_109_small_problems/easy5/easy5_4.rb
+++ 101_109_small_problems/easy5/easy5_4.rb
@@ -5,14 +5,41 @@
 # Examples:
 
 def swap(string)
+  new_string = ''
   string.split(' ').map do |word| 
-    first_letter = word[0]
-    last_letter = word[-1]
-    p first_letter
-    p last_letter
+    word_array = word.chars
+      if word_array.size != 1
+        first_char = word_array[0]
+        last_char = word_array[-1]
+        word_array.pop
+        word_array.shift
+        word_array << first_char
+        word_array.unshift(last_char)
+        word = word_array.join('')
+      end
+    new_string << word + ' '
   end
+  new_string[-1] = ''
+  new_string 
 end
 
 swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
 swap('Abcde') == 'ebcdA'
 swap('a') == 'a'
+
+
+#after reading solution and attempting to mimic it
+
+def swap_first_and_last_char(word)
+  word[0], word[-1] = word[-1], word[0]
+  word 
+end 
+
+def swap(string)
+  result = string.split(' ').map { |word| swap_first_and_last_char(word) }
+  result.join(' ')
+end 
+
+p swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
+p swap('Abcde') == 'ebcdA'
+p swap('a') == 'a'
